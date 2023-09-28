65a8ef02a1f6d320ff98e88c363a8105bd338908
easybi-ms <martin.strekelj@easy.bi>
2023-09-28 22:52:15 +0200

    chore: [webpage] run linter

--- src/navigation.js
+++ src/navigation.js
@@ -3,51 +3,51 @@ import { getPermalink, getAsset } from './utils/permalinks';
 /**
  * Example links
  */
-// 
+//
 
 export const headerData = {
   links: [
     {
-      text: "O projektu",
+      text: 'O projektu',
       links: [
         {
-          text: "Ekipa",
-          href: getPermalink('/wip')
+          text: 'Ekipa',
+          href: getPermalink('/wip'),
         },
         {
-          text: "Prijava na beta testiranje",
-          href: getPermalink('/wip')
+          text: 'Prijava na beta testiranje',
+          href: getPermalink('/wip'),
         },
         {
-          text: "Partnerji",
-          href: getPermalink('/wip')
+          text: 'Partnerji',
+          href: getPermalink('/wip'),
         },
         {
-          text: "Kontakt",
-          href: getPermalink('/wip')
+          text: 'Kontakt',
+          href: getPermalink('/wip'),
         },
       ],
     },
     {
-      text: "Povezave",
+      text: 'Povezave',
       links: [
         {
-          text: "Social Impact Award",
-          href: "https://slovenia.socialimpactaward.net/domov/"
+          text: 'Social Impact Award',
+          href: 'https://slovenia.socialimpactaward.net/domov/',
         },
         {
-          text: "Impact Hub Ljubljana",
-          href: "https://ljubljana.impacthub.net/"
-        }
+          text: 'Impact Hub Ljubljana',
+          href: 'https://ljubljana.impacthub.net/',
+        },
       ],
     },
     {
-      text: "Blog",
-      href: getPermalink('/wip')
+      text: 'Blog',
+      href: getPermalink('/wip'),
     },
     {
-      text: "EkoBuddy trgovina",
-      href: getPermalink('/wip')
+      text: 'EkoBuddy trgovina',
+      href: getPermalink('/wip'),
     },
   ],
   actions: [{ text: 'GLASUJ', href: 'https://slovenia.socialimpactaward.net/', target: '_blank' }],
@@ -76,7 +76,12 @@ export const footerData = {
     // { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/ecobuddy_app/', target: "#blank" },
+    {
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://www.instagram.com/ecobuddy_app/',
+      target: '#blank',
+    },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
@@ -85,116 +90,116 @@ export const footerData = {
 };
 
 // const DEFAULT_LINKS = [
-  //   {
-  //     text: 'Homes',
-  //     links: [
-  //       {
-  //         text: 'SaaS',
-  //         href: getPermalink('/homes/saas'),
-  //       },
-  //       {
-  //         text: 'Startup',
-  //         href: getPermalink('/homes/startup'),
-  //       },
-  //       {
-  //         text: 'Mobile App',
-  //         href: getPermalink('/homes/mobile-app'),
-  //       },
-  //       {
-  //         text: 'Personal',
-  //         href: getPermalink('/homes/personal'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Pages',
-  //     links: [
-  //       {
-  //         text: 'Features (Anchor Link)',
-  //         href: getPermalink('/#features'),
-  //       },
-  //       {
-  //         text: 'Services',
-  //         href: getPermalink('/services'),
-  //       },
-  //       {
-  //         text: 'Pricing',
-  //         href: getPermalink('/pricing'),
-  //       },
-  //       {
-  //         text: 'About us',
-  //         href: getPermalink('/about'),
-  //       },
-  //       {
-  //         text: 'Contact',
-  //         href: getPermalink('/contact'),
-  //       },
-  //       {
-  //         text: 'Terms',
-  //         href: getPermalink('/terms'),
-  //       },
-  //       {
-  //         text: 'Privacy policy',
-  //         href: getPermalink('/privacy'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Landing',
-  //     links: [
-  //       {
-  //         text: 'Lead Generation',
-  //         href: getPermalink('/landing/lead-generation'),
-  //       },
-  //       {
-  //         text: 'Long-form Sales',
-  //         href: getPermalink('/landing/sales'),
-  //       },
-  //       {
-  //         text: 'Click-Through',
-  //         href: getPermalink('/landing/click-through'),
-  //       },
-  //       {
-  //         text: 'Product Details (or Services)',
-  //         href: getPermalink('/landing/product'),
-  //       },
-  //       {
-  //         text: 'Coming Soon or Pre-Launch',
-  //         href: getPermalink('/landing/pre-launch'),
-  //       },
-  //       {
-  //         text: 'Subscription',
-  //         href: getPermalink('/landing/subscription'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Blog',
-  //     links: [
-  //       {
-  //         text: 'Blog List',
-  //         href: getBlogPermalink(),
-  //       },
-  //       {
-  //         text: 'Article',
-  //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-  //       },
-  //       {
-  //         text: 'Article (with MDX)',
-  //         href: getPermalink('markdown-elements-demo-post', 'post'),
-  //       },
-  //       {
-  //         text: 'Category Page',
-  //         href: getPermalink('tutorials', 'category'),
-  //       },
-  //       {
-  //         text: 'Tag Page',
-  //         href: getPermalink('astro', 'tag'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Widgets',
-  //     href: '#',
-  //   },
-  // ]
+//   {
+//     text: 'Homes',
+//     links: [
+//       {
+//         text: 'SaaS',
+//         href: getPermalink('/homes/saas'),
+//       },
+//       {
+//         text: 'Startup',
+//         href: getPermalink('/homes/startup'),
+//       },
+//       {
+//         text: 'Mobile App',
+//         href: getPermalink('/homes/mobile-app'),
+//       },
+//       {
+//         text: 'Personal',
+//         href: getPermalink('/homes/personal'),
+//       },
+//     ],
+//   },
+//   {
+//     text: 'Pages',
+//     links: [
+//       {
+//         text: 'Features (Anchor Link)',
+//         href: getPermalink('/#features'),
+//       },
+//       {
+//         text: 'Services',
+//         href: getPermalink('/services'),
+//       },
+//       {
+//         text: 'Pricing',
+//         href: getPermalink('/pricing'),
+//       },
+//       {
+//         text: 'About us',
+//         href: getPermalink('/about'),
+//       },
+//       {
+//         text: 'Contact',
+//         href: getPermalink('/contact'),
+//       },
+//       {
+//         text: 'Terms',
+//         href: getPermalink('/terms'),
+//       },
+//       {
+//         text: 'Privacy policy',
+//         href: getPermalink('/privacy'),
+//       },
+//     ],
+//   },
+//   {
+//     text: 'Landing',
+//     links: [
+//       {
+//         text: 'Lead Generation',
+//         href: getPermalink('/landing/lead-generation'),
+//       },
+//       {
+//         text: 'Long-form Sales',
+//         href: getPermalink('/landing/sales'),
+//       },
+//       {
+//         text: 'Click-Through',
+//         href: getPermalink('/landing/click-through'),
+//       },
+//       {
+//         text: 'Product Details (or Services)',
+//         href: getPermalink('/landing/product'),
+//       },
+//       {
+//         text: 'Coming Soon or Pre-Launch',
+//         href: getPermalink('/landing/pre-launch'),
+//       },
+//       {
+//         text: 'Subscription',
+//         href: getPermalink('/landing/subscription'),
+//       },
+//     ],
+//   },
+//   {
+//     text: 'Blog',
+//     links: [
+//       {
+//         text: 'Blog List',
+//         href: getBlogPermalink(),
+//       },
+//       {
+//         text: 'Article',
+//         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+//       },
+//       {
+//         text: 'Article (with MDX)',
+//         href: getPermalink('markdown-elements-demo-post', 'post'),
+//       },
+//       {
+//         text: 'Category Page',
+//         href: getPermalink('tutorials', 'category'),
+//       },
+//       {
+//         text: 'Tag Page',
+//         href: getPermalink('astro', 'tag'),
+//       },
+//     ],
+//   },
+//   {
+//     text: 'Widgets',
+//     href: '#',
+//   },
+// ]
